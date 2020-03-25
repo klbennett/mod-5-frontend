d05ac03fd10a2c69aa837b5fb59b44347909078d
Kay Bennett <kaybennett@kbanddigital.home>
2020-03-25 15:51:01 +0000

    changed css import

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { history } from "./helpers";
 import { alertActions } from "./actions";
 import Notifications, { notify } from "react-notify-toast";
 import { hot } from "react-hot-loader";
-import "bulma/bulma.css";
+import "./styles.css";
 import Nav from "./containers/Nav";
 import * as action from "./actions";
 import Footer from "./components/Footer";
